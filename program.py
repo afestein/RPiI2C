ed7b893cfca42b3b97945811591130c7811a1ad4
afestein <afestein@gmail.com>
2019-04-22 12:52:06 +1000

    add loop

--- program.py
+++ program.py
@@ -1,9 +1,3 @@
-# Distributed with a free-will license.
-# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
-# TSL2561
-# This code is designed to work with the TSL2561_I2CS I2C Mini Module available from ControlEverything.com.
-# https://www.controleverything.com/content/Light?sku=TSL2561_I2CS#tabs-0-product_tabset-2
-
 import smbus
 import time
 
@@ -21,19 +15,22 @@ bus.write_byte_data(0x39, 0x01 | 0x80, 0x02)
 
 time.sleep(0.5)
 
-# Read data back from 0x0C(12) with command register, 0x80(128), 2 bytes
-# ch0 LSB, ch0 MSB
-data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
+while 1:
+    # Read data back from 0x0C(12) with command register, 0x80(128), 2 bytes
+    # ch0 LSB, ch0 MSB
+    data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
 
-# Read data back from 0x0E(14) with command register, 0x80(128), 2 bytes
-# ch1 LSB, ch1 MSB
-data1 = bus.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
+    # Read data back from 0x0E(14) with command register, 0x80(128), 2 bytes
+    # ch1 LSB, ch1 MSB
+    data1 = bus.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
 
-# Convert the data
-ch0 = data[1] * 256 + data[0]
-ch1 = data1[1] * 256 + data1[0]
+    # Convert the data
+    ch0 = data[1] * 256 + data[0]
+    ch1 = data1[1] * 256 + data1[0]
 
-# Output data to screen
-print("Full Spectrum(IR + Visible) :%d lux" % ch0)
-print("Infrared Value :%d lux" % ch1)
-print("Visible Value :%d lux" % (ch0 - ch1))
+    # Output data to screen
+    print("Full Spectrum(IR + Visible) :%d lux" % ch0)
+    print("Infrared Value :%d lux" % ch1)
+    print("Visible Value :%d lux" % (ch0 - ch1))
+
+    time.sleep(1)
